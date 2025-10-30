4c41c4900ada094419d0e7031c161f195081dba1
Do Hoang Huong Lien <liendhhha140217@gmail.com>
2025-10-30 09:38:06 +0700

    Framework setup

---
+++ src/main/java/com/example/lingdingdong/config/WebSocketConfig.java
@@ -0,0 +1,18 @@
+package com.example.lingdingdong.config;
+
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/topic");
+        registry.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws").setAllowedOrigins("*");
+    }
+}

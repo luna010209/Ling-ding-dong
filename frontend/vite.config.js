4953682c1dd3826f30b7000be8b152b695360485
Do Hoang Huong Lien <liendhhha140217@gmail.com>
2025-11-02 22:40:52 +0900

    Lingdingdong frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5173,
+    proxy: {
+      '/ws': 'http://localhost:8080', // proxy websocket
+      '/api': 'http://localhost:8080', // if you add REST APIs
+    },
+  },
 })
